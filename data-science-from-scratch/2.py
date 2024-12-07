63f36f5d5e6808643b322a41155108974758bbf5
Arek <arek@wilamowski.net>
2024-12-07 13:58:49 +0100

    feat(data-science-scratch): add pythonc practice

--- data-science-from-scratch/2.py
+++ data-science-from-scratch/2.py
@@ -93,3 +93,96 @@ except KeyError:
 
 joels_grade = grades.get("Joel", 0)
 p(joels_grade)
+
+grades["Tim"] = 99
+p(len(grades))
+tweet = {
+    "user" : "joelgrus",
+    "text" : "data science bla bla"
+}
+p(tweet)
+
+p(grades.keys())
+p(tweet.values())
+p(tweet.items())
+p("user" in tweet.keys())
+p("user" in tweet)
+p("joelgrus" in tweet.values())
+# word counter
+text = "test blabla test test test blabla"
+
+document = text.split(" ")
+
+word_counts = {}
+for word in document:
+    if word in word_counts:
+        word_counts[word] +=1
+    else:
+        word_counts[word] = 1
+p(word_counts)
+
+# word coiunter v2
+word_counts2 = {}
+for word in document:
+    try:
+        word_counts2[word] +=1
+    except KeyError:
+        word_counts2[word] = 1
+# v3
+word_counts3 = {}
+for word in document:
+    previous_count = word_counts3.get(word,0) # if doesn't exists then set default value
+    word_counts3[word] = previous_count + 1
+
+# defaultdics
+from collections import defaultdict
+word_counts4 = defaultdict(int)
+for word in document:
+    word_counts4[word] +=1
+
+dd_list = defaultdict(int) #
+p(dd_list)
+dd_list = defaultdict(list) # empty list
+
+dd_list[2].append(1)
+p("dd_list: ", dd_list)
+dd_dict = defaultdict(dict) # empty map
+dd_dict["Joel"]["City"] = "Seattle"
+
+dd_pair = defaultdict(lambda: [0,0])
+dd_pair[2][1] = 1
+# Counter
+from collections import Counter
+c = Counter([0,1,2])
+word_counts5 = Counter(document)
+
+for word, count in word_counts5.most_common(10):
+    print(word, count)
+
+# Sets, unique values
+primes_below_10 = {2,3,5,7}
+s = set()
+s.add(1)
+s.add(2)
+s.add(2)
+p(s) # 1,2
+
+x = len(s)
+p(x)
+
+y=2 in s
+z = 3 in s
+
+stopwords_list = ["a", "an" ,"at"]
+p("zip" in stopwords_list)
+p("a" in stopwords_list)
+
+stopwords_set = set(stopwords_list)
+p("zip" in stopwords_set)
+
+item_list = [1,2,3,1,2,3]
+num_items = len(item_list)
+p(num_items)
+item_set = set(item_list)
+p(item_set)
+
