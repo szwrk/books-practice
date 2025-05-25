5e807c481dc8d13fb69bccfd810255bbf93bf0a6
Arek <arek@wilamowski.net>
2025-05-26 00:34:59 +0200

    fix typo

---
+++ sql-advanced-queries/credentials-testdb.sh
@@ -0,0 +1,4 @@
+export ORACLE_USER="test"
+export ORACLE_PASSWORD="oracle"
+export ORACLE_HOST="srv2.lan"
+export ORACLE_SERVICE="free"
---
+++ sql-advanced-queries/readme.md
@@ -0,0 +1,4 @@
+# Readme
+
+![Over Example](assets/sql_over.apng)
+![Over keep partition Example](assets/sql_overkeeppart.apng)
